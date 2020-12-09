4c02c317bcfb4d69bcb26ffba3e7207f242d28ab
David Rask <david.rask@hotmail.com>
2020-12-09 14:36:09 +0100

    Add project files.

---
+++ Inlämningsuppgift/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Inlämningsuppgift
+{
+    class Program
+     //TODO
+     /* Display the keys in console
+      * Create sounded notes
+      * a, a#, b, c, c#, d, d#, e, f, f#. g, g#
+      * navigate "up" the scale of the "keys"
+
+     //TODO:
+     // gå höger/vänster för att välja mini-tangent
+     // space så spelar man tangenten man är på (alla låter lika dant)
+     // '12 tone equal temprament' för att ha rätt Hz på tangenterna
+     // pröva OOP, ett objekt per tangent som har ett 'Hz' fält och en char för hur tangenten ser ut
+     // lägg alla tangent objekt i en lista och rita upp dem via att loopa listan
+     // skapa större tangenter
+      */
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+
+            // string[] note_names = new string[12];
+            // note_names[0] == Console.Beep(220, 1000)
+            // note_names[1] == Console.Beep(233, 1000)
+            // note_names[2] == Console.Beep(247, 1000)
+            // note_names[3] == Console.Beep(262, 1000)
+            // note_names[4] == Console.Beep(277, 1000)
+            // note_names[5] == Console.Beep(294, 1000)
+            // note_names[6] == Console.Beep(311, 1000)
+            // note_names[8] == Console.Beep(330, 1000)
+            // note_names[9] == Console.Beep(349, 1000)
+            // note_names[10] == Console.Beep(370, 1000)
+            // note_names[11] == Console.Beep(392, 1000)
+            // note_names[12] == Console.Beep(415, 1000)
+            
+            /*
+            char[,] canvas = new char[3, 3];
+            int x = 1;
+            int y = 1;
+            canvas[x, y] = '*';
+
+            // ___   [0,0] [1,0] [2,0]
+            // _*_   [0,1] [1,1] [2,1]
+            // ___   [0,2] [1,2] [2,2]
+            */
+
+        }
+    }
+}
